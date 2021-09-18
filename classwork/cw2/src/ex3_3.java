0225600e0c3283da0e08471b4e6215f806335571
Tymofii <60606068+tantisch@users.noreply.github.com>
2021-09-18 16:01:01 +0300

    Add files via upload

---
+++ classwork/cw2/src/ex3_3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ex3_3{
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = console.nextLine();
+        System.out.println("Hello, " + name);
+    }
+}
