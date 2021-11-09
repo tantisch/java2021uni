917ef3737d0194c78d76f7fbeb89a8f8caf7bc4b
Tymofii <60606068+tantisch@users.noreply.github.com>
2021-11-09 22:04:21 +0200

    Add files via upload

---
+++ homework/hw8/ex3_7.java
@@ -0,0 +1,59 @@
+package hw8;
+
+import hw8.Triangle;
+
+public class ex3_7{
+    public static void main(String[] args){
+        Triangle[] triangles = new Triangle[]{ new Triangle(0, 0, 0), new Triangle(1, 2, 3), new Triangle(5, 5, 5), new Triangle(3, 4, 5), new Triangle(2, 2, 4), new Triangle(6, 6, 6)};
+        int right = 0;
+        int ind_of_biggest_area_right = 0;
+        int scalene = 0;
+        int ind_of_biggest_area_scalene = 0;
+        int isosceles = 0;
+        int ind_of_biggest_area_isosceles = 0;
+        int equilateral = 0;
+        int ind_of_biggest_area_equilateral = 0;
+
+        for(int i = 0; i < triangles.length; i++){
+            if (triangles[i].is_right()){
+                right++;
+                if (triangles[i].count_area() >= triangles[ind_of_biggest_area_right].count_area()){
+                    ind_of_biggest_area_right = i;
+                }
+
+            }
+
+            else if (triangles[i].is_scalene()){
+                scalene++;
+                if (triangles[i].count_area() >= triangles[ind_of_biggest_area_scalene].count_area()){
+                    ind_of_biggest_area_scalene = i;
+                }
+            }
+
+            else if (triangles[i].is_isosceles()){
+                isosceles++;
+                System.out.println(triangles[i]);
+                if (triangles[i].count_area() >= triangles[ind_of_biggest_area_isosceles].count_area()){
+                    ind_of_biggest_area_isosceles = i;
+                }
+            }
+
+            else if (triangles[i].is_equilateral()){
+                equilateral++;
+                if (triangles[i].count_area() >= triangles[ind_of_biggest_area_equilateral].count_area()){
+                    ind_of_biggest_area_equilateral = i;
+                }
+            }
+        }
+
+        System.out.println("Amount of right triangles: " + right);
+        System.out.println("Amount of scalene triangles: " + scalene);
+        System.out.println("Amount of isosceles triangles: " + isosceles);
+        System.out.println("Amount of equilateral triangles: " + equilateral);
+
+        System.out.println("Biggest area of right triangles has: " + triangles[ind_of_biggest_area_right]);
+        System.out.println("Biggest area of scalene triangles has: " + triangles[ind_of_biggest_area_scalene]);
+        System.out.println("Biggest area of isosceles triangles has: " + triangles[ind_of_biggest_area_isosceles]);
+        System.out.println("Biggest area of equilateral triangles has: " + triangles[ind_of_biggest_area_equilateral]);
+    }
+}
